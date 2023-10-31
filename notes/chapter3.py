71d1975936d31069de87604e4d8f083b0696970c
dowens2013 <dowens2013@gmail.com>
2023-10-31 19:01:04 -0400

    10/31 notes for chapter 3

---
+++ notes/chapter3.py
@@ -0,0 +1,9 @@
+currentGames = ['Overwatch', 'Starfield', 'Warzone', 'Cyberpunk 2077']
+print(currentGames)
+
+currentGames[2] = 'Red Dead Redemption II'
+print(currentGames)
+
+currentGames.append('Overwatch 2')
+print(currentGames)
+
